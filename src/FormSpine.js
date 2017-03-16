250ac42fa50eb0f7ea40dd4a58ee4c36248590c8
Lasse Rafn <lasserafn@gmail.com>
2017-03-16 20:18:46 +0100

    Fixes

--- src/FormSpine.js
+++ src/FormSpine.js
@@ -1,20 +1,18 @@
-const Validator = require('./Validator');
-const ErrorBag = require('./ErrorBag');
-const fetch = require('unfetch');
+// var Validator = require('./Validator');
+// var ErrorBag = require('./ErrorBag');
+var fetch = require('unfetch');
 
-class FormSpine {
-	constructor(url, fields, customErrorMessages, clearOnSuccess) {
-		this.errors = new ErrorBag;
+function FormSpine(url, fields, customErrorMessages, clearOnSuccess) {
+		// this.errors = new ErrorBag;
 		this.setupFields(fields);
 		this.url = url;
-		this.validator = new Validator(customErrorMessages);
+		// this.validator = new Validator(customErrorMessages);
 		this.clearOnSuccess = clearOnSuccess !== undefined ? clearOnSuccess : false;
-	};
 
-	setupFields(fields) {
+	this.setupFields = function(fields) {
 		this.fields = {};
 		this.originalValues = {};
-		for (let field in fields) {
+		for (var field in fields) {
 			fields[field]["value"] = fields[field].value ? fields[field].value : "";
 			fields[field]["name"] = field;
 
@@ -23,36 +21,36 @@ class FormSpine {
 		}
 	};
 
-	validate() {
+	this.validate = function() {
 		this.errors.clear();
 		return this.validator.validate(this.fields);
 	};
 
-	data() {
-		let formData = {};
-		for (let field in this.fields) {
+	this.data = function() {
+		var formData = {};
+		for (var field in this.fields) {
 			formData[field] = this.fields[field].value;
 		}
 		return formData;
 	};
 
-	clear() {
-		for (let field in this.fields) {
+	this.clear = function() {
+		for (var field in this.fields) {
 			this.fields[field].value = "";
 		}
 		this.errors.clear();
 	};
 
-	reset() {
-		for (let field in this.fields) {
+	this.reset = function() {
+		for (var field in this.fields) {
 			this.fields[field].value = this.originalValues[field];
 		}
 		this.errors.clear();
 	};
 
-	submit(method) {
-		return new Promise((resolve, reject) => {
-			const validationResponse = this.validate();
+	this.submit = function(method) {
+		return new Promise(function(resolve, reject) {
+			var validationResponse = this.validate();
 
 			if (Object.keys(validationResponse).length > 0) {
 				this.errors.set(validationResponse);
@@ -60,18 +58,20 @@ class FormSpine {
 
 				return false;
 			}
-			fetch[method](this.url, this.data()).then(response => {
+			fetch[method](this.url, this.data()).then(function(response) {
 				this.onSuccess(response.data);
 				resolve(response.data);
 
 				return true;
 			}).catch((error) => {
-				let responseError = "";
+				var responseError = "";
+
 				if (error.response !== undefined && error.response.data !== undefined && typeof error.response.data === "object") {
 					responseError = error.response.data;
 				} else {
 					responseError = error.response.statusText;
 				}
+
 				this.onFail(responseError);
 				reject(responseError);
 
@@ -80,13 +80,13 @@ class FormSpine {
 		});
 	};
 
-	onSuccess(data) {
+	this.onSuccess = function(data) {
 		if (this.clearOnSuccess) {
 			this.reset();
 		}
 	};
 
-	onFail(errors) {
+	this.onFail = function(errors) {
 		if (typeof errors === "string") {
 			errors = {
 				general: [errors]
@@ -95,15 +95,15 @@ class FormSpine {
 		this.errors.set(errors);
 	};
 
-	post() {
+	this.post = function() {
 		return this.submit("post");
 	};
 
-	delete() {
+	this.delete = function() {
 		return this.submit("delete");
 	};
 
-	put() {
+	this.put = function() {
 		return this.submit("put");
 	};
 }
