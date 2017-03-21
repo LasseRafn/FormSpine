da1801534cdf38edf0b5e6f533430e0fcd5dc688
Lasse Rafn <lasserafn@gmail.com>
2017-03-21 18:43:44 +0100

    Fixes

--- index-builder.js
+++ index-builder.js
@@ -3,38 +3,38 @@ var fs = require('fs');
 var validatorData,
 	errorBagData;
 
+/*
+ * This is a ugly mess.
+ * Todo: Fix later
+ */
+
 fs.readFile('src/Validator.js', 'utf8', function (err, data) {
 	if (err) {
 		return console.log(err);
 	}
 
 	validatorData = data;
-});
 
-fs.readFile('src/ErrorBag.js', 'utf8', function (err, data) {
-	if (err) {
-		return console.log(err);
-	}
+	fs.readFile('src/ErrorBag.js', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
 
-	errorBagData = data;
-});
+		errorBagData = data;
+	});
 
-/*
- * This is a ugly mess.
- * Todo: Fix later
- */
+	var result = "require('unfetch/polyfill');\n";
 
-var result = "require('unfetch/polyfill');\n";
+	fs.readFile('src/FormSpine.js', 'utf8', function (err, data) {
+		if (err) {
+			return console.log(err);
+		}
 
-fs.readFile('src/FormSpine.js', 'utf8', function (err, data) {
-	if (err) {
-		return console.log(err);
-	}
+		result += data.replace('const Validator = require("./Validator");', validatorData)
+			.replace('const ErrorBag = require("./ErrorBag");', errorBagData);
 
-	result += data.replace('const Validator = require("./Validator");', validatorData)
-		.replace('const ErrorBag = require("./ErrorBag");', errorBagData);
-
-	fs.writeFile('index.js', result, 'utf8', function (err) {
-		if (err) return console.log(err);
+		fs.writeFile('index.js', result, 'utf8', function (err) {
+			if (err) return console.log(err);
+		});
 	});
 });
